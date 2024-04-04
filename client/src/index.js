fbe06af4ffda1f0291cfdfff29171e5ed0c14b26
Wilson <imweesheng@gmail.com>
2024-04-04 11:22:32 +0800

    Add reducers setup and auth reducers

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+
 import App from "./components/App";
+import reducers from "./reducers";
+
+const store = createStore(() => reducers, {}, applyMiddleware());
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
