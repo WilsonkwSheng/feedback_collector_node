e2d2ccfc46e0661185aa18aa981885070ce4e291
Wilson <imweesheng@gmail.com>
2024-04-04 13:56:00 +0800

    Setup thunk to middleware and fetchUser function

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import { thunk } from "redux-thunk";
 
 import App from "./components/App";
 import reducers from "./reducers";
 
-const store = createStore(() => reducers, {}, applyMiddleware());
+const store = createStore(() => reducers, {}, applyMiddleware(thunk));
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
