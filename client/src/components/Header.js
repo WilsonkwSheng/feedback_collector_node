02982598974cd90ff2d03add3b790006737dd94b
Wilson <imweesheng@gmail.com>
2024-04-04 12:35:00 +0800

    Add materialize css package and add basic styling to header

---
+++ client/src/components/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <nav>
+        <div class="nav-wrapper">
+          <a class="brand-logo">Emaily</a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li>
+              <a>Login With Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Header;
