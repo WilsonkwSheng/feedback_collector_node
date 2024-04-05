b10a4b940014698e10b2e5662fb6b197612d1aed
Wilson <imweesheng@gmail.com>
2024-04-05 14:58:11 +0800

    Update env keys for stripe

--- config/prod.js
+++ config/prod.js
@@ -3,4 +3,6 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.GOOGLE_MONGO_URI,
   cookieKey: process.env.GOOGLE_COOKIE_KEY,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
 };
